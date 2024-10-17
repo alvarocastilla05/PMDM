c6aadda7b7ad654651222fd537448bf8ceab106c
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2024-10-17 11:57:25 +0200

    Ejercicio Barra de progreso Terminado

--- ANGULAR/barraProgreso/src/app/components/barra/barra.component.ts
+++ ANGULAR/barraProgreso/src/app/components/barra/barra.component.ts
@@ -10,9 +10,18 @@ export class BarraComponent {
   @Input() porcentaje = 80;
 
  
-  get getAncho() {
-    return `${this.porcentaje}%`;
+  getProgressColor(){
+    if(this.porcentaje <= 25){
+      return 'danger';
+    }
+    if(this.porcentaje > 25 && this.porcentaje <= 50){
+      return 'warning';
+    }if(this.porcentaje > 50 && this.porcentaje <= 75){
+      return 'info';
+    }
+    return 'success';
   }
+ 
   
 
   
