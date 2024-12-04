0c762d40c3a3800a18a8326a74048d523128704f
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-12-04 14:39:48 +0100

    Lo consegui

--- Flutter/app_casa/lib/login-page.dart
+++ Flutter/app_casa/lib/login-page.dart
@@ -6,17 +6,20 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Page')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add)),
-      body: const Column(
-        children: [
-          ElevatedButton(
-            onPressed: null, child: Text('Login')
-          ),
-          Text('Estamos en el login page'),
-        ],
-      ));
+      appBar: AppBar(title: const Text('Login page')),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      body: Column(children: [
+        Container(
+          color: Colors.blue,
+          width: double.infinity,
+          child: const ElevatedButton(onPressed: null, child: Text("Login")),
+        ),
+        Container(
+            color: const Color.fromARGB(255, 0, 255, 115),
+            width: double.infinity,
+            child: const Text("Estamos en el login page")),
+      ]),
+    );
   }
 }
