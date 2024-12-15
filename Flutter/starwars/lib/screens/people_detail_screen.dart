3f0ab21cfc811f2db59d6217a876ce2afc23c2c8
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-12-15 02:32:08 +0100

    Iconos añadidos

--- Flutter/starwars/lib/screens/people_detail_screen.dart
+++ Flutter/starwars/lib/screens/people_detail_screen.dart
@@ -10,33 +10,58 @@ class PeopleDetailScreen extends StatefulWidget {
 }
 
 class _PeopleDetailScreenState extends State<PeopleDetailScreen> {
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.peopleItem.name!.toLowerCase(), style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
-          backgroundColor: Colors.black,
-        ),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(30), // Ajusta el radio según tus necesidades
-                  child: Image.network(
-                    'https://starwars-visualguide.com/assets/img/characters/${_extractIdFromUrl(widget.peopleItem.url!)}.jpg',
-                  ),
+      appBar: AppBar(
+        title: Text(widget.peopleItem.name!.toLowerCase(), style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30), // Ajusta el radio según tus necesidades
+                child: Image.network(
+                  'https://starwars-visualguide.com/assets/img/characters/${_extractIdFromUrl(widget.peopleItem.url!)}.jpg',
                 ),
-                Text('Name: ${widget.peopleItem.name}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
-                Text('Height: ${widget.peopleItem.height}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
-                Text('Mass: ${widget.peopleItem.mass}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
-                Text('Hair Color: ${widget.peopleItem.hairColor}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
-              ]
-            ),
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.person, color: Colors.yellow),
+                  const SizedBox(width: 8),
+                  Text('Name: ${widget.peopleItem.name}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
+                ],
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.height, color: Colors.yellow),
+                  const SizedBox(width: 8),
+                  Text('Height: ${widget.peopleItem.height}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
+                ],
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.fitness_center, color: Colors.yellow),
+                  const SizedBox(width: 8),
+                  Text('Mass: ${widget.peopleItem.mass}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
+                ],
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.color_lens, color: Colors.yellow),
+                  const SizedBox(width: 8),
+                  Text('Hair Color: ${widget.peopleItem.hairColor}', style: const TextStyle(color: Colors.yellow, fontFamily: 'StarJedi')),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   String _extractIdFromUrl(String url) {
