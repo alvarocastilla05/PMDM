e3addd1c70ffd89839f8c41590720cd896f71c41
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2024-12-17 10:50:24 +0100

    Movie list

--- Flutter/tmdb/lib/main.dart
+++ Flutter/tmdb/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tmdb/MainMenuScreen.dart';
+import 'package:tmdb/movieScreen.dart';
 
 
 void main() {
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Mainmenuscreen(),
+      home: const Moviescreen(),
     );
   }
 }
