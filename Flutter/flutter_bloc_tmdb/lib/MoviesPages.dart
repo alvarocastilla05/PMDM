e2c23d7e4e6b5db5125eef81598edca472053f7b
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2024-12-19 13:30:26 +0100

    Comienzo explicacion bloc

---
+++ Flutter/flutter_bloc_tmdb/lib/MoviesPages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Moviespage extends StatefulWidget {
+  const Moviespage({super.key});
+
+  @override
+  State<Moviespage> createState() => _MoviespagesState();
+}
+
+class _MoviespagesState extends State<Moviespage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
